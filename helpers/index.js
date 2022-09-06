c7b02f1004aac279e30e9f617823d667747db403
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-09-06 20:06:47 +0300

    :rainbow:

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,11 @@
-const { readLocalFile } = require('./file');
+const axios = require('axios');
+const { createExchangeObject } = require('./object');
 
-const getExchangeRates = () => readLocalFile();
+const getExchangeRates = async () => {
+    const { data } = await axios.get(process.env.TCMB_API_URL);
+
+    return createExchangeObject(data);
+};
 
 module.exports = {
     getExchangeRates
