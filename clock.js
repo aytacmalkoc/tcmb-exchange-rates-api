54c28aa8427b50bf1a6c801e26700082f3994174
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-09-06 18:11:57 +0300

    Test clock.js

--- clock.js
+++ clock.js
@@ -7,17 +7,17 @@ const { createExchangeObject } = require('./helpers/object');
 
 // Run this job every day at 15:30
 
-const fetchUpdates = new CronJob('*/5 * * * *', async () => {
+const fetchUpdates = new CronJob('*/2 * * * *', async () => {
     try {
         const { data } = await axios.get('https://www.tcmb.gov.tr/kurlar/today.xml');
         const exchangeRates = createExchangeObject(data)
 
         if (!isLocalFileU2D(exchangeRates.last_update.bulletin_no)) {
             writeLocalFile(exchangeRates);
-            console.log('writing');
+            console.log('writing: ', exchangeRates);
         }
     } catch (error) {
-        console.log(error.message);
+        console.error(error.message);
     }
 }, null, true, 'Europe/Istanbul');
 
