6afb965252fb4a9b33ded503de01906e31ccdb83
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-09-07 00:57:51 +0300

    Add encodeURI :unicorn:

--- helpers/index.js
+++ helpers/index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { createExchangeObject } = require('./object');
 
 const getExchangeRates = async () => {
-    const { data } = await axios.get(process.env.TCMB_API_URL);
+    const { data } = await axios.get(encodeURI(process.env.TCMB_API_URL));
 
     return createExchangeObject(data);
 };
